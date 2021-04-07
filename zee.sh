1bc8b3d643b4ec37e24f21a060829640009d9b0d
Zeetoyah <82073700+Zeetoyah@users.noreply.github.com>
2021-04-07 11:58:58 +0700

    Add files via upload

---
+++ zee.sh
@@ -0,0 +1,10 @@
+pkg update
+echo "sript by ZeeChrist"
+pkgupgrade
+echo "sript by ZeeChrist"
+pkg install git
+echo "sript by ZeeChrist"
+pkg install nodejs
+echo "sript by ZeeChrist"
+git clone http://github.com/zennn08/BaseSelfBot
+echo "sript by ZeeChrist"
